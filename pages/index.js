9e53e9b9f75a8b9cd892ef3361be6d4199b3afd5
Paulo Vitor Souza <paulo225vitor@gmail.com>
2021-04-06 23:22:43 -0300

    Tutorial: navigation

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>PVS DEV</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post" >
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
