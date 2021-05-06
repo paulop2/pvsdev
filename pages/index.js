6a22f896752d12274254c96a9b4cbd11c4cebd3f
Paulo Vitor Souza <paulo225vitor@gmail.com>
2021-05-06 11:28:54 -0300

    Standartization of the project

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -12,14 +12,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Read{' '}
-          <Link href="/posts/first-post" >
+          Read
+          {' '}
+          <Link href="/posts/first-post">
             <a>this page!</a>
           </Link>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing
+          {' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -60,10 +62,11 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by
+          {' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
