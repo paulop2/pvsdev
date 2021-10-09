e08e9aec47a10239017995436ca0b882284b2140
Paulo Vitor Souza <paulo225vitor@gmail.com>
2021-10-08 23:39:32 -0300

    Retirada da imagem no footer

--- pages/index.js
+++ pages/index.js
@@ -62,9 +62,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by
-          {' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by Vercel
         </a>
       </footer>
     </div>
